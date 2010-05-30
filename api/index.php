81b684baa8033efaebb731f848bb50ac73aeaa40
Magnus Enger <magnus@enger.(none)>
2010-05-30 18:02:52 +0000

    Update api/index.php to use glitre_search()

--- api/index.php
+++ api/index.php
@@ -6,12 +6,24 @@ $config = get_config('hig');
 
 // Search
 if (!empty($_GET['q'])) {
-	echo(glitre_search($_GET['q']));
+  $args = array(
+    'q' => $_GET['q'], 
+    'library' => $_GET['library'], 
+    'format' => $_GET['format'], 
+    'page' => $_GET['page'],
+    'per_page' => $_GET['per_page']
+  );
+  echo(glitre_search($args));
 }
 
 // Display one record	
 if (!empty($_GET['id'])) {
-	echo(glitre_record($_GET['id']));
+  $args = array(
+    'id' => $_GET['id'], 
+    'library' => $_GET['library'], 
+    'format' => $_GET['format'], 
+  );
+  echo(glitre_search($args));
 }
 
 ?>
