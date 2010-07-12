fb3b4b87989c604d9226da7eda01d5036ec8d58d
Magnus Enger <magnus@enger.priv.no>
2010-07-12 13:40:27 +0200

    More dummy data

--- api/index.php
+++ api/index.php
@@ -6,18 +6,29 @@ include('../inc.glitre.php');
 if (!empty($_GET['dummy'])) {
 
   if (!empty($_GET['q'])) {
-    echo('
-    <ul class="rounded">
-    <li><a class="flip" href="#dummyresult1">Treff 1 for ' . $_GET['q'] . '</a></li>
-    <li><a class="flip" href="#dummyresult2">Treff 2</a></li>
-    <li><a class="flip" href="#dummyresult3">Treff 3</a></li>
-    </ul>
-    ');
-    exit;
+    if (!empty($_GET['format'])) {
+      echo('
+      <div id="dummyresult1"><div class="toolbar"><h1>Demotittel 1</h1><a class="button back" href="#">Tilbake</a></div><div><ul><li>Tittel: Demotittel 1</li><li>Forfatter: Demoforfatter</li><li>Utgitt: 1999</li></ul></div></div>
+      <div id="dummyresult2"><div class="toolbar"><h1>Demotittel 2</h1><a class="button back" href="#">Tilbake</a></div><div><ul><li>Tittel: Demotittel 2</li><li>Forfatter: Demoforfatter</li><li>Utgitt: 1998</li></ul></div></div>
+      <div id="dummyresult3"><div class="toolbar"><h1>Demotittel 3</h1><a class="button back" href="#">Tilbake</a></div><div><ul><li>Tittel: Demotittel 3</li><li>Forfatter: Demoforfatter</li><li>Utgitt: 1997</li></ul></div></div>
+      ');
+      exit;
+    } else {
+      echo('
+      <h2>Treffliste</h2>
+      <ul class="rounded">
+      <li><a class="flip searchresult" href="#dummyresult1">Treff 1 for ' . $_GET['q'] . '</a></li>
+      <li><a class="flip searchresult" href="#dummyresult2">Treff 2</a></li>
+      <li><a class="flip searchresult" href="#dummyresult3">Treff 3</a></li>
+      </ul>
+      ');
+      exit;
+    }
   }
 
   if (!empty($_GET['id'])) {
-    echo(glitre_search($args));
+    echo('test');
+    exit;
   }
 
 }
