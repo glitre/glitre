3d6c326ddc9999ecac66ad17e9ca1a7cd2d5d784
Magnus Enger <magnus@enger.(none)>
2010-08-02 14:12:26 +0000

    Add doctype, encoding etc to testing/zsort.php

--- testing/zsort.php
+++ testing/zsort.php
@@ -1,6 +1,10 @@
-<html>
+<?php echo('<?xml version="1.0" encoding="utf-8"?>'); echo("\n\n"); ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="nb_NO" lang="nb_NO">
 <head>
 <title>Zsort</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <body>
 
