73450f2220ca50d7c6033d5fe9294a9db7fcc0b9
Magnus Enger <magnus@enger.(none)>
2011-04-15 14:24:39 +0000

    First working version of oppnabib.php (more or less)

--- plugins/oppnabib.php
+++ plugins/oppnabib.php
@@ -7,6 +7,8 @@ function oppnabib_detail($record, $style, $loggedin_user) {
 	
   global $config;
 
+  // Extract some data about the record
+  // These will be sent to Oppna bibliotek if the book is not there yet
   $title = '';
   if ($record->getField("245") && $record->getField("245")->getSubfield("a")) {
     $title = urlencode(marctrim($record->getField("245")->getSubfield("a")));
@@ -20,81 +22,138 @@ function oppnabib_detail($record, $style, $loggedin_user) {
       list($authorlastname, $authorfirstname) = explode(", ", $author);
     }
   }
-  // DEBUG exit("$authorlastname $authorfirstname");
+  
+  $isbn = '';
+  if ($record->getField("020") && $record->getField("020")->getSubfield("a")) {
+    $isbn = marctrim($record->getField("020")->getSubfield("a"));
+  }
   
   // Get the ID of the book
   $idurl = $config['plugins']['oppnabib']['baseurl'] . "books?book[title]=$title&book[authorfirstname]=$authorfirstname&book[authorlastname]=$authorlastname";
   $book = simplexml_load_string(getOppnabib($idurl, true));
-  $id = $book->book->id;
+  $bookid = $book->book->id;
   
-  // Get any data about the book identified by the ID
-  $dataurl = $config['plugins']['oppnabib']['baseurl'] . "assessments?assessment[book_id]=" . $id;
-  $bookdata = simplexml_load_string(getOppnabib($dataurl));
+  if ($bookid) {
+    // Get any data about the book identified by the ID
+    $dataurl = $config['plugins']['oppnabib']['baseurl'] . "assessments?assessment[book_id]=" . $bookid;
+    $bookdata = simplexml_load_string(getOppnabib($dataurl));
+  }
   
   $out = '';
+  $new_assessment_added = false;
   
   if ($style == 'full') {
   
-	  $out = '<div class="oppnabib"><h2>Öppna bibliotek - ' . urldecode($title) . '</h2>';
+	  $out = '<div class="oppnabib"><h2>Öppna bibliotek - ' . urldecode($title) . '</h2>' . "\n";
+	  $out .= '<!-- bookid: ' . $bookid . ' -->' . "\n";
 	  
+	  // Do we have alogged in user? 
 	  if (!empty($loggedin_user)) {
+	  	
 	  	  // Check if a user has added an opinion
-	  	  if (!empty($_POST['oppnabib_assessment_header_input'])) {
+	  	  if (!empty($_POST['oppnabib_assessment_header_input'])
+	  	   || !empty($_POST['oppnabib_assessment_text_input']) 
+	  	   || !empty($_POST['oppnabib_grade_input'])) {
+	  	  	
 	  	  	  $header = strip_tags($_POST['oppnabib_assessment_header_input']);
 	  	  	  $text = strip_tags($_POST['oppnabib_assessment_text_input']);
 	  	  	  // check that the grade is an integer, set to zero otherwise
 	  	  	  $grade = 0;
-	  	  	  if (is_int(strip_tags($_POST['oppnabib_grade_input']))) {
+	  	  	  if (is_int((int) strip_tags($_POST['oppnabib_grade_input']))) {
+	  	  	  	// TODO: check that $grade is between 1 and 6
 	  	  	    $grade = $_POST['oppnabib_grade_input'];
 	  	  	  } 
-	  	  	  // This is probably not needed, since the user's input will be in the list below
-	  	  	  $out .= format_assessment($header, $text, $grade, $loggedin_user);
+	  	  	  // Collect the data that will be sent to Oppna bibliotek
+	  	  	  $book = array();
+	  	  	  if (!empty($bookid) && !empty($bookdata)) {
+	  	  	    // This book is already in Oppna bibliotek, so we only need to send the ID
+			    $book['assessment[grade]']= $grade;
+			    $book['assessment[comment_header]']= $header;
+			    $book['assessment[comment_text]']= $text;
+			    $book['assessment[book_id]']= $bookid;
+			    $book['assessment[published]'] = 1;
+	  	      } else {
+	  	  	    // This book is not in Oppna bibliotek, we need to add it
+			    $book['assessment[grade]'] = $grade;
+			    $book['assessment[comment_header]'] =  $header;
+			    $book['assessment[comment_text]'] = $text;
+			    $book['assessment[title]'] = $title;
+			    $book['assessment[authorfirstname]'] = $authorfirstname;
+			    $book['assessment[authorlastname]'] = $authorlastname;
+			    $book['assessment[isbn]'] = $isbn;
+			    $book['assessment[published]'] = 1;
+	  	  	 }
+	  	  	 $newassessment = simplexml_load_string(postOppnabib($book, $loggedin_user));
+	  	  	 if ($newassessment->error) {
+	  	  	    $out .= '<p class="error">Noe gikk galt: ' . "\n";
+	  	  	    $out .= '"' . $newassessment->error . '"</p>' . "\n";
+				$report = print_r($assessment, true);
+	  	  	 	$out .= '<!-- ' . $report . ' -->';
+	  	  	 } else {
+	  	  	 	$out .= '<p>Takk for at du bidro med din vurdering!</p>' . "\n";
+				$out .= format_assessment($header, $text, $grade, $loggedin_user);
+				$report = print_r($newassessment, true);
+	  	  	 	$out .= '<!-- ' . $report . ' -->';
+	  	  	 	$new_assessment_added = true;
+	  	  	 }
+	  	  	  
+	  	  // Create the form for adding an assessment  
 	  	  } else {
-			  $out .= '<h3>Si din mening</h3>';
-			  $out .= '<p>Du er logget inn som: ' . $loggedin_user . '</p>';
-			  $out .= '<form method="post">';
-			  $out .= 'Omtalens tittel:<br /><input type="text" name="oppnabib_assessment_header_input" size="40" /><br />';
-			  $out .= 'Omtale:<br /><textarea name="oppnabib_assessment_text_input" cols="40" rows="10" ></textarea><br />';
-			  $out .= '<select name="oppnabib_grade_input">';
-			  $out .= '<option value="">Gi karakter</option>';
-			  $out .= '<option value="1">&#10029;</option>';
-			  $out .= '<option value="2">&#10029;&#10029;</option>';
-			  $out .= '<option value="3">&#10029;&#10029;&#10029;</option>';
-			  $out .= '<option value="4">&#10029;&#10029;&#10029;&#10029;</option>';
-			  $out .= '<option value="5">&#10029;&#10029;&#10029;&#10029;&#10029;</option>';
-			  $out .= '<option value="5">&#10029;&#10029;&#10029;&#10029;&#10029;&#10029;</option>';
-			  $out .= '</select><br />';
-			  $out .= '<input type="submit">';
-			  $out .= '</form>';
+	  	  	
+			  $out .= '<h3>Si din mening</h3>' . "\n";
+			  $out .= '<p>Du er logget inn som: ' . $loggedin_user . '</p>' . "\n";
+			  $out .= '<form method="post">' . "\n";
+			  $out .= 'Omtalens tittel:<br /><input type="text" name="oppnabib_assessment_header_input" size="40" /><br />' . "\n";
+			  $out .= 'Omtale:<br /><textarea name="oppnabib_assessment_text_input" cols="40" rows="10" ></textarea><br />' . "\n";
+			  $out .= '<select name="oppnabib_grade_input">' . "\n";
+			  $out .= '<option value="">Gi karakter</option>' . "\n";
+			  $out .= '<option value="1">&#10029;</option>' . "\n";
+			  $out .= '<option value="2">&#10029;&#10029;</option>' . "\n";
+			  $out .= '<option value="3">&#10029;&#10029;&#10029;</option>' . "\n";
+			  $out .= '<option value="4">&#10029;&#10029;&#10029;&#10029;</option>' . "\n";
+			  $out .= '<option value="5">&#10029;&#10029;&#10029;&#10029;&#10029;</option>' . "\n";
+			  $out .= '<option value="6">&#10029;&#10029;&#10029;&#10029;&#10029;&#10029;</option>' . "\n";
+			  $out .= '</select><br />' . "\n";
+			  $out .= '<input type="submit">' . "\n";
+			  $out .= '</form>' . "\n";
 	  	  }
 	  }
 	  
-	  $out .= '<h3>Hva andre mener</h3>';
+	  // Display existing assessments
+	  $out .= '<h3>Hva andre mener</h3>' . "\n";
 	  
 	  // $out .= '<p>Antall meninger: ' . $bookdata->hitcount . '</p>';
 	  if ($bookdata->average_grade) {
-	    $out .= '<p>Gjennomsnittlig karakter: <span class="oppnabib_average_grade">' . $bookdata->average_grade . '</span></p>';
+	    $out .= '<p>Gjennomsnittlig karakter: <span class="oppnabib_average_grade">' . $bookdata->average_grade . '</span></p>' . "\n";
 	  } else {
-	  	$out .= '<p>Bli den første til å mene noe om denne boka!</p>';
+	  	if ($new_assessment_added == false) {
+	  	  $out .= '<p>Bli den første til å mene noe om denne boka!</p>' . "\n";
+	  	}
 	  }
 	  
 	  // Output every assessment
-	  foreach ($bookdata->assessment as $assessment) {
-	    $out .= '<p class="oppnabib_assessment">';
-	    $out .= format_assessment($assessment->comment_header, $assessment->comment_text, $assessment->grade, $assessment->username);
-	    $out .= '(<span class="oppnabib_user_lib_name">' . $assessment->user_lib_name . '</span>)';
-	    $out .= "</p>\n";
+	  if ($bookdata->assessment) {
+  	    foreach ($bookdata->assessment as $thisassessment) {
+	      $out .= '<p class="oppnabib_assessment">' . "\n";
+	      $out .= format_assessment($thisassessment->comment_header, $thisassessment->comment_text, $thisassessment->grade, $thisassessment->username);
+	      $out .= '(<span class="oppnabib_user_lib_name">' . $thisassessment->user_lib_name . '</span>)' . "\n";
+	      $out .= "</p>\n";
+	    }
 	  }
 	  
-	  $out .= '</div>';
+	  $out .= '</div>' . "\n";
 
   } elseif ($style == 'compact') {
   	
-    $out .= '<br /><span class="oppnabib_compact">Öppna bibliotek: <span class="oppnabib_hitcount">' . $bookdata->hitcount . '</span> mening(er). ';
+  	$hitcount = 0;
+  	if ($bookdata->hitcount) {
+  		$hitcount = $bookdata->hitcount;
+  	}
+    $out .= '<br /><span class="oppnabib_compact">Öppna bibliotek: <span class="oppnabib_hitcount">' . $hitcount . '</span> mening(er). ' . "\n";
     if ($bookdata->average_grade) {
-      $out .= 'Gjennomsnitt: <span class="oppnabib_average_grade">' . $bookdata->average_grade . '</span>';
+      $out .= 'Gjennomsnitt: <span class="oppnabib_average_grade">' . $bookdata->average_grade . '</span>' . "\n";
     } 
-    $out .= '</span>';
+    $out .= '</span>' . "\n";
   	
   }
   
@@ -111,8 +170,9 @@ function format_assessment($header, $text, $grade, $loggedin_user) {
 	if ($header != '' || $text != '') {
 		if ($header != '') {
 			$out .= '<strong class="oppnabib_assessment_header">' . $header . '</strong> ';
+			if ($text != '') { $out .= '<br />'; }
 		}
-		if ($header != '') {
+		if ($text != '') {
 			$out .= '<span class="oppnabib_assessment_text">' . $text . '</span>';
 		}
 		$out .= "<br />\n";
@@ -124,26 +184,62 @@ function format_assessment($header, $text, $grade, $loggedin_user) {
 	
 }
 
-function getOppnabib($url, $auth = false) {
+// Add a new assessment
+function postOppnabib($data, $user) {
+	
+  global $config;
 
-  // Session
-  // session_start();
-  // if(isset($_SESSION['login']) ) {
-  //   session_unregister($_SESSION);
-  //   session_unset();
-  // }
+  $curl_opt = array(
+    CURLOPT_URL            => $config['plugins']['oppnabib']['baseurl'] . 'assessments', 
+    CURLOPT_POST           => true,
+    CURLOPT_RETURNTRANSFER => true,     
+    CURLOPT_HEADER         => false,    
+    CURLOPT_FOLLOWLOCATION => true,     
+    CURLOPT_ENCODING       => "",       
+    CURLOPT_USERAGENT      => "Glitre", 
+    CURLOPT_AUTOREFERER    => true,     
+    CURLOPT_CONNECTTIMEOUT => 120,      
+    CURLOPT_TIMEOUT        => 120,      
+    CURLOPT_MAXREDIRS      => 10,       
+    CURLOPT_HTTPHEADER     => array("Accept: application/xml"), 
+    CURLOPT_HTTPAUTH       => CURLAUTH_BASIC, 
+    CURLOPT_USERPWD        => $user . ":" . $config['plugins']['oppnabib']['userpassword'], 
+    CURLOPT_POSTFIELDS     => $data, 
+    CURLINFO_HEADER_OUT    => true, # for debug purposes
+  );
+  
+  $ch = curl_init();
+  curl_setopt_array($ch, $curl_opt);
+  
+  $content = curl_exec($ch);
+  
+  // Debug
+  // $info = curl_getinfo($ch);
+  // print_r($info);
+  
+  if (curl_errno($ch)) {
+  	exit(curl_error($ch));
+  } else {
+    return $content;
+  }
+  curl_close($ch);
+	
+}
+
+// Retrieve an existing assessment
+function getOppnabib($url, $auth = false) {
 
   $curl_opt = array(
     CURLOPT_URL            => $url, 
-    CURLOPT_RETURNTRANSFER => true,     // return web page
-    CURLOPT_HEADER         => false,    // don't return headers
-    CURLOPT_FOLLOWLOCATION => true,     // follow redirects
-    CURLOPT_ENCODING       => "",       // handle all encodings
+    CURLOPT_RETURNTRANSFER => true,     
+    CURLOPT_HEADER         => false,    
+    CURLOPT_FOLLOWLOCATION => true,     
+    CURLOPT_ENCODING       => "",       
     CURLOPT_USERAGENT      => "Glitre", 
-    CURLOPT_AUTOREFERER    => true,     // set referer on redirect
-    CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
-    CURLOPT_TIMEOUT        => 120,      // timeout on response
-    CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
+    CURLOPT_AUTOREFERER    => true,     
+    CURLOPT_CONNECTTIMEOUT => 120,      
+    CURLOPT_TIMEOUT        => 120,      
+    CURLOPT_MAXREDIRS      => 10,       
     CURLOPT_HTTPHEADER     => array("Accept: application/xml"), 
     CURLOPT_HTTPAUTH       => CURLAUTH_BASIC, 
   );
@@ -156,13 +252,18 @@ function getOppnabib($url, $auth = false) {
     curl_setopt($ch, CURLOPT_USERPWD, $config['plugins']['oppnabib']['username'] . ":" . $config['plugins']['oppnabib']['password']);
   }
 
+  $content = curl_exec($ch);
+  
+  // Debug
+  // $info = curl_getinfo($ch);
+  // print_r($info);
+  
   if (curl_errno($ch)) {
   	exit(curl_error($ch));
   } else {
-    $content = curl_exec( $ch );
     return $content;
   }
-  curl_close( $ch );
+  curl_close($ch);
 	
 }
 
